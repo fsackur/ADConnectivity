f0812fc809f2c8727fb034f71da1bb1a36c4971c
freddie <fsackur@users.noreply.github.com>
2017-04-30 12:25:26 +0100

    Updated pre-commit hook script

--- Private/Pre-Commit.ps1
+++ Private/Pre-Commit.ps1
@@ -1,10 +1,52 @@
-#!/bin/sh
+
+<#
+    Contents of .git\hooks\pre-commit (no file extension):
+
+    powershell -NoProfile -ExecutionPolicy Bypass -File '$GIT_DIR\..\Private\Pre-Commit.ps1'
+
+    (This expects this file to be in Private subfolder of repo. Update accordingly)
+
+    $GIT_DIR is a bash environment variable pointing to the .git folder. 'pre-commit' is a bash script.
+
+    The git hook fails the commit if exit code from this PS is non-zero.
+#>
+
+$ModuleName = 'ADConnectivity'
+
+#Script expects to be in subfolder of repo
 Set-Location $PSScriptRoot\..
+if (-not (Test-Path .git)) {
+    Write-Output "Pre-Commit.ps1: not in expected location"
+    exit 1
+}
 
-$ManifestVersion = (Get-Module .\ADConnectivity.psd1 -ListAvailable).Version
-if (-not $ManifestVersion) {exit 1}
+$ManifestPath = ".\$ModuleName.psd1"
 
-$NewVersion = $ManifestVersion | %{New-Object version ($_.Major, $_.Minor, $_.Build, ($_.Revision+1))}
+#Get staged changes in .psd1 file
+$StatusDiff = (git status $ManifestPath -v) -join "`n"
 
-Update-ModuleManifest -Path .\ADConnectivity.psd1 -ModuleVersion $NewVersion
-git add .\ADConnectivity.psd1
+if ($StatusDiff -match "-ModuleVersion = '[\d\.]+?'\n\+ModuleVersion = '[\d\.]+?'") {
+    Write-Output 'Pre-Commit.ps1: ModuleVersion has already been changed in manifest file; skipping auto-increment'
+    exit 0
+
+
+} else {
+    #Auto-increment version
+    try {
+        $Manifest = (Get-Module $ManifestPath -ListAvailable)
+    } catch {
+        Write-Output "Pre-Commit.ps1: module manifest not in expected location"
+    }
+
+    $ManifestVersion = $Manifest.Version
+    if (-not $ManifestVersion) {
+        Write-Output "Pre-Commit.ps1: version not present in module manifest"
+        exit 1
+
+    } else {
+        $NewVersion = $ManifestVersion | %{New-Object version ($_.Major, $_.Minor, $_.Build, ($_.Revision+1))}
+        Write-Output 'Pre-Commit.ps1: Incremented manifest version'
+        Update-ModuleManifest -Path $ManifestPath -ModuleVersion $NewVersion
+        git add $ManifestPath
+    }
+}
