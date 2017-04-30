93e5f278219a0a835ba2dc8cf08a6e06ac75968f
freddie <fsackur@users.noreply.github.com>
2017-04-30 15:33:54 +0100

    Formatting tests

--- Tests/Formatting.Pester.Tests.ps1
+++ Tests/Formatting.Pester.Tests.ps1
@@ -7,13 +7,29 @@ Import-Module $ModulePath -Force
 Describe $ModuleName {
     
     Context 'Functionality' {
+        $MyObject = New-Object psobject -Property @{
+            Material="Wood";
+            Size=15;
+            FearFactor=9;
+            ComfortLevel=12;
+            Id=(New-Guid).Guid
+        }
 
-        It 'Works' {
+        It 'Adds TypeName' {
+            $MyObject | Add-DefaultMembers -TypeName 'Chair'
+            $MyObject.PSTypeNames[0] | Should BeExactly 'Chair'
+        }
+
+        It 'Adds default display properties' {
+            $MyObject | Add-DefaultMembers -DisplayProperties 'Material', 'Size'
+            $MyObject.PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames | Should BeExactly 'Material', 'Size'
         }
 
 
-
-
+        It 'Adds default sort properties' -Pending {
+            $MyObject | Add-DefaultMembers -SortProperties 'Material', 'Size'
+            $MyObject.PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames | Should BeExactly 'Material', 'Size'
+        }
 
 
     }
