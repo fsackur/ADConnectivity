38f64b9e51b895db10f107bec0d43f70846f9434
freddie <fsackur@users.noreply.github.com>
2017-04-30 13:22:47 +0100

    Added skeleton tests

---
+++ Tests/ADConnectivity.Pester.Tests.ps1
@@ -0,0 +1,22 @@
+﻿
+$ModuleName = $MyInvocation.MyCommand -replace '\..*' #-replace 'Pester\.' -replace 'Tests\.' -replace 'ps1$', 'psm1'
+$ModulePath = "$PSScriptRoot\..\$ModuleName"
+Import-Module $ModulePath -Force
+
+
+Describe $ModuleName {
+    
+    Context 'Functionality' {
+
+        It 'Works' {
+        }
+
+
+
+
+
+
+    }
+}
+
+Remove-Module $ModuleName
---
+++ Tests/Invoke-Nltest.Pester.Tests.ps1
@@ -0,0 +1,22 @@
+﻿
+$ModuleName = $MyInvocation.MyCommand -replace '\..*' #-replace 'Pester\.' -replace 'Tests\.' -replace 'ps1$', 'psm1'
+$ModulePath = "$PSScriptRoot\..\Public\$ModuleName"
+Import-Module $ModulePath -Force
+
+
+Describe $ModuleName {
+    
+    Context 'Functionality' {
+
+        It 'Works' {
+        }
+
+
+
+
+
+
+    }
+}
+
+Remove-Module $ModuleName
